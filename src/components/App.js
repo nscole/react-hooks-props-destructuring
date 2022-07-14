a54da00f707cd1c28ca4656649d6be5dc296123e
Naomi Cole <naomisarahcole@gmail.com>
2022-07-14 18:03:40 +1000

    Updated examples

--- src/components/App.js
+++ src/components/App.js
@@ -9,9 +9,21 @@ function App() {
 
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      <MovieCard title={title} posterSrc={posterURL} genres={genresArr}/>
     </div>
   );
 }
+
+// function App() {
+//   const title = "Mad Max";
+//   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
+
+//   return (
+//     <div className="App">
+//       {/* posterSrc is omitted, so the default value will be used instead */}
+//       <MovieCard title={title} genres={genresArr} />
+//     </div>
+//   );
+// } 
 
 export default App;
